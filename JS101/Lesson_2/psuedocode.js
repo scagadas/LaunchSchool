3455ab4032864f08bd010620f440296617da6da8
Sean Cagadas <scagadas@gmail.com>
2023-03-03 23:10:06 -0800

    JS101 lesson examples

---
+++ JS101/Lesson_2/psuedocode.js
@@ -0,0 +1,23 @@
+// a function that returns the sum of two numbers
+// pseudocode:
+// Given a set of intergers
+// Set the integers to variables, num1 and num2
+// Use the addition method to add the two variables
+
+
+// a function that takes an array of strings and returns
+// a string that is all those strings concatinated together
+// pseudocode:
+// create a new variable that is empty
+// write a function that iterates over each element in the array
+// to use .concat method to join each element to the new empty variable
+
+
+// a method that takes an array of integers, and returns a new array
+// with every other element from the orginal array
+// starting with the first element
+// pseudocode:
+// create an empty array and set it to a new variable
+// starting at element 0
+// write a function that will iterate every other element
+// For each iteration, add the the element to the new array
