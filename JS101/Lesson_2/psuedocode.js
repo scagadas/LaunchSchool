c0d0b2b903dfb1b1ca9ec6fcf4cfcfd324f2b19a
Sean Cagadas <scagadas@gmail.com>
2023-03-05 14:54:24 -0800

    Adding Eslint to the workspace

--- JS101/Lesson_2/psuedocode.js
+++ JS101/Lesson_2/psuedocode.js
@@ -21,3 +21,20 @@
 // starting at element 0
 // write a function that will iterate every other element
 // For each iteration, add the the element to the new array
+
+// a function that determines the index of thre 3rd occurence
+// of a given character in a string.
+// pseudocode:
+// use loop statement to iterate over every index
+// where it will add one each time the character matches
+// If the count does not reach 3, then have the function return null
+
+// a function that takes two arrays of numbers and returns
+// the result of merging the arrays.
+// the elements of the first array are the even indexes
+// the elements of the second array are the odd indexes
+// pseudocode:
+// create an empty array
+// have a loop run where it iterates over the first array
+// pushing each element into the even indexes
+// another loop does the same for the odd indexes
