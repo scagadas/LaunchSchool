3ed2f495a241d090fbdc74678452f1525794ca0d
Sean Cagadas <scagadas@gmail.com>
2023-03-16 22:27:01 -0700

    bunch of JS101 Easy exercises

--- JS101/Lesson_2/rock_paper_scissors.js
+++ JS101/Lesson_2/rock_paper_scissors.js
@@ -1,44 +1,75 @@
-// a rock, paper, scissors game
+// a rock,paper scissors game
 // there are two opponents, the player and a computer
-// If player a chooses rock and player b chooses scissors, player a wins.
+// If player a chooses rock and player b chooses scissors  player a wins.
 // If player a chooses paper and player b chooses rock, player a wins.
-// If player a chooses scissors and player b chooses paper, player a wins.
+// If player a chooses scissors and player b chooses paper player a wins.
 // If both players choose the same item, neither player wins. It's a tie.
 // game flow:
 // The user makes a choice
 // the computer makes a choice
 // winner is displayed
+let r = 'rock';
+let p = 'paper';
+let sc = 'scissors';
+let sp = 'spock';
+let l =  'lizard';
 
+const choices = {
+	rock : ['rock', 'r'],
+	paper : ['paper', 'p'],
+	scissors : ['scissors', 'sc'],
+	spock : ['spock', 'sp'],
+	lizard : ['lizard', 'l'],
+};
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = Object.keys(choices);
+
+let playerScore = 0;
+let computerScore = 0;
+
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-while (true) {
-  prompt(`Choose your move: ${VALID_CHOICES.join(", ")}`);
-  let choice = readline.question();
 
-  while (!VALID_CHOICES.includes(choice)) {
+function declareScore () {
+  prompt(`The total score is: Player Score:${playerScore}, Computer Score:${computerScore}`);
+}
+
+while (true) {
+  prompt(`Choose your move: ${VALID_CHOICES.join(`, `)}`);
+  let playerChoice = readline.question();
+
+  while (!Object.values(choices).flat(1).includes(playerChoice)) {
     prompt("That's not a valid choice");
-    choice = readline.question();
+    playerChoice = readline.question();
   }
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  prompt(`You chose ${choice}, computer chose ${computerChoice}.`);
+  prompt(`You chose ${playerChoice}, computer chose ${computerChoice}.`);
 
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-      (choice === 'paper' && computerChoice === 'rock') ||
-      (choice === 'scissors' && computerChoice === 'paper')) {
+  if ((playerChoice === r && computerChoice === sc) ||
+      (playerChoice === r && computerChoice === l) ||
+      (playerChoice === p && computerChoice === r) ||
+      (playerChoice === p && computerChoice === sp) ||
+      (playerChoice === sc && computerChoice === p) ||
+      (playerChoice === sc && computerChoice === l) ||
+      (playerChoice === sp && computerChoice === r) ||
+      (playerChoice === sp && computerChoice === sc) ||
+      (playerChoice === l && computerChoice === p) ||
+      (playerChoice === l && computerChoice === sp)) {
     prompt('You win!');
-  } else if ((choice === 'scissors' && computerChoice === 'rock') ||
-             (choice === 'rock' && computerChoice === 'paper') ||
-             (choice === 'paper' && computerChoice === 'scissors')) {
-    prompt('Computer wins!');
-  } else {
+    declareScore();
+    playerScore++;
+  } else if (playerChoice === computerChoice) {
     prompt("It's a tie");
+    declareScore();
+  } else {
+    prompt("computer wins!");
+    computerScore++;
+    declareScore();
   }
 
   prompt ('Do you want to play again (y/n)?');
---
+++ JSExercises/JS101-JS119/example-charCodeAt.js
@@ -0,0 +1,5 @@
+
+let value = 3
+let newValue = (value > 0 ? -value : value);
+
+console.log(newValue);
